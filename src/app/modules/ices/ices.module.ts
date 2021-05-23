8b1790aa71a84371e0b30f1698cca97ab0598266
ckechad <ckechad@icloud.com>
2021-05-23 16:27:53 +0200

    feat(modules/ngrx)

---
+++ src/app/modules/ices/ices.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IcesRoutingModule } from './ices-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, IcesRoutingModule],
+})
+export class IcesModule {}
